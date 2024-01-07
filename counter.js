d60a104948ee95cdc3cfdf429dfea333149eef7b
LisaOsmanova <lischen.osmanova@gmail.com>
2024-01-08 01:50:14 +0200

    firat step

--- counter.js
+++ counter.js
@@ -1,9 +1,63 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+let ids = [];
+let score = 0;
+let countScore = document.querySelector(".score");
+let svgs = [];
+
+document.querySelector(".container")
+    .addEventListener("click",(event)=> {
+        const stopPressing = ids.length >= 2;
+        if (!stopPressing){
+            const clickedButton = event.target;
+            
+            let svg = clickedButton.querySelector("svg");
+            console.log(svg);
+            svgs.push(svg);
+            console.log(svgs);
+
+            if (svgs.length > 0) {
+                showPicture(svgs);
+            }
+
+            //to get data-id
+            let id = clickedButton.getAttribute("data-id");
+            
+            // showPicture(svg);
+            ids.push(id);
+
+            let timetoCount = (ids.length === 2);
+            if (timetoCount){
+                let [firstId, secondId] = ids;
+                handleId(firstId, secondId, svgs);
+            }
+        }
+});
+
+function showPicture(list) {
+    for (let svg of list) {
+        svg.classList.remove("hidden");
+    }
+}
+
+function hidePicture(list) {
+    for (let svg of list) {
+        svg.classList.add("hidden");
+    }
+    svgs = [];
+    console.log(svgs);
+}
+
+function handleId (firstId, secondId,list) {
+    if (firstId === secondId) {
+        score = score + 2;
+        countScore.textContent = `score: ${score}`;
+        setTimeout(() => {
+            hidePicture(list);
+        }, 1000);
+    } else {
+        setTimeout(() => {
+            hidePicture(list);
+        }, 1000);
+    }
+
+    ids = [];
 }
